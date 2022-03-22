d6951561e17230e1f380ff5cc8f05ffc23f73647
henriqueyujiandrade <henrique_yujiandrade@hotmail.com>
2022-03-22 10:21:24 -0300

    feat api

---
+++ src/js/db/api.js
@@ -0,0 +1,18 @@
+class Api{
+    static url = 'https://kenzie-food-api.herokuapp.com/products'
+
+    static produtos(){
+    const response = await fetch(`${this.postUrl}`, {
+        'method': 'GET',
+        'headers': {
+            'Content-Type': 'application/json',
+            
+        },
+    })
+
+    const responseData = await response.json()
+
+    return responseData
+}
+
+}
