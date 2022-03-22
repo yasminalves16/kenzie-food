7ea287a6a556ee359aed3b13e149df57b3d93f25
Victoria Viana <victoriagarcia963@gmail.com>
2022-03-22 20:27:32 -0300

    feat-filtro da barra de pesquisa

---
+++ src/js/models/filtro-teste.js
@@ -0,0 +1,29 @@
+class Busca {
+    static pesquisarProdutos(data, pesquisa){
+
+        const campoBusca = data.filter((produto) => {
+
+            const {nome, categoria} = produto
+
+            const pesquisaFormatada = pesquisa.toLowerCase()
+            const nomeProdutoFormatado = nome.toLowerCase()
+            const categoriaProdutoFormatado = categoria.toLowerCase()
+
+            if(nomeProdutoFormatado.includes(pesquisaFormatada) || categoriaProdutoFormatado.includes(pesquisaFormatada)){
+                return produto
+            }
+
+        })
+        
+        return campoBusca
+    }
+
+    static filtroCategoria(data, value){
+        const filtrarCategoria = data.filter((produto) => {
+            return produto.categoria === value
+        })
+        return filtrarCategoria
+    }
+}
+
+export {Busca}
